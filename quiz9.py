a243cd074fc4944f4d07c95cc0fb3f05cf77564e
ChanUi <hcu55@naver.com>
2023-06-24 20:40:20 +0900

    quiz10 풀이 및 quiz9 오류 수정

--- quiz9.py
+++ quiz9.py
@@ -25,37 +25,36 @@ def myremove(c):        # 문자가 삭제될 때 호출되는 함수
     global checkList, myList, checkSecret
     if c == 'A':
         if myList[0] == checkList[0]:
-            checkSecret[0] -= 1
+            checkSecret -= 1 
         myList[0] -= 1
     elif c == 'C':
         if myList[1] == checkList[1]:
-            checkSecret[1] -= 1
+            checkSecret -= 1
         myList[1] -= 1
     elif c == 'G':
         if myList[2] == checkList[2]:
-            checkSecret[2] -= 1
+            checkSecret -= 1
         myList[2] -= 1
     elif c == 'T':
         if myList[3] == checkList[3]:
-            checkSecret[3] -= 1
+            checkSecret -= 1
         myList[3] -= 1
 
 S, P = map(int, input().split())
-Result = 0
+Result = 0          # 최종 유효한 비밀번호 개수
 A = list(input())
 checkList = list(map(int, input().split()))
 
-for i in range(4):
+for i in range(4):          # {'A','C','G','T'} 중 최소 개수가 0개 이면 무조건 조건을 만족하므로 checkSecret에 1 더해줌
     if checkList[i] == 0:
         checkSecret += 1
 
-for i in range(P):
+for i in range(P):          # 윈도우 크기만큼 앞에서부터 먼저 탐색
     myadd(A[i])
-
-if checkSecret == 4:
+if checkSecret == 4:    # 만약 체크리스트 결과를 만족하면 최종 개수 +1
     Result += 1
 
-for i in range(P, S):
+for i in range(P, S):       # 윈도우를 오른쪽으로 이동 시키며 i는 추가 하는 문자열 j는 삭제하는 문자열  
     j = i - P
     myadd(A[i])
     myremove(A[j])
@@ -63,4 +62,3 @@ for i in range(P, S):
         Result += 1
 
 print(Result)
-    
