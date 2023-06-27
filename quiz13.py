d1e469722e1b76a4348e3956890b85ef0438f30f
ChanUi <hcu55@naver.com>
2023-06-27 20:53:03 +0900

    quiz13

--- quiz13.py
+++ quiz13.py
@@ -1,19 +1,15 @@
 import sys
-<<<<<<< HEAD
 input = sys.stdin.readline
-=======
-input = sys.stdin.readline
-from collections import deque
+from collections import deque   # 덱을 사용하는 라이브러리 가져오기
 
 N = int(input())
 myQueue = deque()
 
-for i in range(1, N+1):
+for i in range(1, N+1):     # 덱에 1부터 추가
     myQueue.append(i)
 
-while len(myQueue) > 1:
-    myQueue.popleft()
-    myQueue.append(myQueue.popleft())
+while len(myQueue) > 1:     # 내 덱에 하나가 남을 때까지 반복
+    myQueue.popleft()       # 맨 아래 카드를 뽑기 위해 popleft
+    myQueue.append(myQueue.popleft())       # 그 다음 아래 카드를 뽑고 맨 위로 추가
 
-print(myQueue.pop())
->>>>>>> e566c9a (quiz13)
+print(myQueue.pop())    # 한장이 남았을 때 추출
