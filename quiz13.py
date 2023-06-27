0807a13a169e424dda469517da76661294660fe0
ChanUi <hcu55@naver.com>
2023-06-27 20:49:22 +0900

    quiz13

--- quiz13.py
+++ quiz13.py
@@ -1,2 +1,19 @@
 import sys
+<<<<<<< HEAD
 input = sys.stdin.readline
+=======
+input = sys.stdin.readline
+from collections import deque
+
+N = int(input())
+myQueue = deque()
+
+for i in range(1, N+1):
+    myQueue.append(i)
+
+while len(myQueue) > 1:
+    myQueue.popleft()
+    myQueue.append(myQueue.popleft())
+
+print(myQueue.pop())
+>>>>>>> e566c9a (quiz13)
