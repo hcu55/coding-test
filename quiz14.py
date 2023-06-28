5c03409ade964af1b4d1728bd8f42e8cbee32c71
ChanUi <hcu55@naver.com>
2023-06-28 19:48:48 +0900

    quiz14 풀이

--- quiz14.py
+++ quiz14.py
@@ -1,3 +1,18 @@
+from queue import PriorityQueue
 import sys
 input = sys.stdin.readline
+print = sys.stdout.write        
 
+N = int(input())
+myQueue = PriorityQueue()
+
+for i in range(N):
+    request = int(input())
+    if request == 0:
+        if myQueue.empty():
+            print('0\n')
+        else:
+            temp = myQueue.get()        # 우선순위 큐의 값 삭제해서 temp변수에 넣음
+            print(str(temp[1])+'\n')    # temp변수에 (x,y) 형태니까 y값인 1번 출력
+    else:
+        myQueue.put((abs(request), request)) # 절댓값을 기준으로 정렬하고 같으면 음수 우선 정렬하도록 구성
