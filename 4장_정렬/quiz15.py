2a2e5e678d22bee7258fac637c24121e8e8a0889
ChanUi <hcu55@naver.com>
2023-06-28 21:15:10 +0900

    quiz15 풀이

--- 4장_정렬/quiz15.py
+++ 4장_정렬/quiz15.py
@@ -1,4 +1,9 @@
 N = int(input())
+
+# A = [0] * N           # 반복문으로 입력을 받을 수도 있지만 아래와 같이 한번에 입력을 받을 수 있다.
+# for i in range(N):    
+#     A[i] = int(input())
+
 A = [int(input()) for _ in range(N)] 
 
 for i in range(N-1):    # N개의 수를 두개씩 비교하면 N-1번 반복하게 됨
