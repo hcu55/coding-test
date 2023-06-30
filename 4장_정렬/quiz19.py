4b0556aa07999ddee85549e65afe5a5574a2d169
ChanUi <hcu55@naver.com>
2023-06-30 22:12:45 +0900

    quiz19

---
+++ 4장_정렬/quiz19.py
@@ -0,0 +1,11 @@
+def solution(k, n, reqs):
+    answer = 0
+    
+    for i in range(len(reqs)):
+        print(reqs[i][2])
+
+
+
+
+
+solution(2,3,[[5,55,2],[10,90,2],[20,40,2],[50,45,2],[100,50,2]])
