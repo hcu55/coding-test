d24a49e70fbebe06f6e2427c7b189df11ef1ba65
ChanUi <hcu55@naver.com>
2023-07-01 22:51:39 +0900

    quiz19

--- 4장_정렬/quiz19.py
+++ 4장_정렬/quiz19.py
@@ -1,11 +1,7 @@
-def solution(k, n, reqs):
-    answer = 0
-    
-    for i in range(len(reqs)):
-        print(reqs[i][2])
+import sys
+input = sys.stdin.readline
 
+N = int(input())
+A = list(map(int, input().split))
 
-
-
-
-solution(2,3,[[5,55,2],[10,90,2],[20,40,2],[50,45,2],[100,50,2]])
+mylist = []
